3d4faf373fc1b660e45b2d98acf227d3b300dcf0
f-a-l-c-o-d-e <178407688+f-a-l-c-o-d-e@users.noreply.github.com>
2024-09-01 22:17:06 +0200

    feat(main.js): Add the getHumanChoice function
    Added the getHumanChoice function, which returns
    the player choice between: "rock", "paper",
    "scissors"

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Create the function "getComputerChoice"
 function getComputerChoice() {
     // Pick a random int number between 0 and 2 and save it in "choice"
-    choice = Math.round(Math.random() * 2)
+    const choice = Math.round(Math.random() * 2)
     // If "choice" is 0 then
     if (choice === 0) {
         // Return "rock"
@@ -14,11 +14,26 @@ function getComputerChoice() {
     } else if (choice === 2) {
         // Return "scissors"
         return "scissors"
-    // In any other cases
+    // In any other case
     } else {
         // Return "Error: choice is not an int number between 0 and 2"
         return "Error: choice is not an int number between 0 and 2"
     // End else
     }
+// End the function
+}
+
+// Create the function getHumanChoice
+function getHumanChoice() {
+    // Store the user choice in "choice"
+    let choice = prompt("Type your choice (rock, paper or scissors):")
+    // If "choice" is not "rock", "paper" or "scissors" then
+    if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+        // Remake the choice and save it in "choice"
+        choice = getHumanChoice()
+    // End if
+    }
+    // Return "choice"
+    return choice
 // End the function
 }
