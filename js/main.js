3c0e6f2eae125f3be573a6edd71e32a624f28870
f-a-l-c-o-d-e <178407688+f-a-l-c-o-d-e@users.noreply.github.com>
2024-09-08 15:45:25 +0200

    style(main.js): Delete some comments

--- js/main.js
+++ js/main.js
@@ -1,56 +1,30 @@
-// Create the function "getComputerChoice"
 function getComputerChoice() {
-    // Pick a random int number between 0 and 2 and save it in "choice"
     const choice = Math.round(Math.random() * 2)
-    // If "choice" is 0 then
     if (choice === 0) {
-        // Return "rock"
         return "rock"
-    // If instead "choice" is 1 then
     } else if (choice === 1) {
-        // Return "paper"
         return "paper"
-    // If instead "choice" is 2 then
     } else if (choice === 2) {
-        // Return "scissors"
         return "scissors"
-    // In any other case
     } else {
-        // Return "Error: choice is not an int number between 0 and 2"
         return "Error: choice is not an int number between 0 and 2"
-    // End else
     }
-// End the function
 }
 
-// Create the function "getHumanChoice"
 function getHumanChoice() {
-    // Store the user choice in "choice"
     let choice = prompt("Type your choice (rock, paper or scissors):")
-    // Transform "choice" to lower case
     choice = choice.toLowerCase()
-    // If "choice" is not "rock", "paper" or "scissors" then
     if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
-        // Remake the choice and save it in "choice"
         choice = getHumanChoice()
-    // End if
     }
-    // Return "choice"
     return choice
-// End the function
 }
 
-// Create the function "playGame"
+// Based on "numberOfRound" initialize the game
 function playGame(numberOfRound) {
-    // Create "humanScore" variable and set it to 0
     let humanScore = 0
-    // Create "computerScore" variable and set it to 0
     let computerScore = 0
-    // Create the function "playRound", which takes two parameters: "humanChoice", "computerChoice"
     function playRound(humanChoice, computerChoice) {
-        // Create "winner" variable and set it to ""
-        let winner = ""
-
         // Based on "humanChoice" and "computerChoice" return the winner as "human", "computer" or "no one"
         function getWinner (humanChoice, computerChoice) {
             if (humanChoice === computerChoice) {
@@ -64,7 +38,7 @@ function playGame(numberOfRound) {
             }
         }
 
-        winner = getWinner(humanChoice,computerChoice)
+        const winner = getWinner(humanChoice,computerChoice)
 
         // Based on "winner", "humanScore" and "computerScore" update "humanScore" and "computerScore"
         function updateScore (winner) {
@@ -102,6 +76,7 @@ function playGame(numberOfRound) {
         updateScoreAndShowWinner(winner)
     }
 
+    // Make the number of rounds equal to "numberOfRound"
     for (let i = 1; i <= numberOfRound; i++) {
         playRound(getHumanChoice(),getComputerChoice())
     }
